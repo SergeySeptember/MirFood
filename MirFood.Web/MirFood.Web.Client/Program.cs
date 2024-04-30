a5f71d1085b99d193834c24907ad0d8311e85d91
SergeySeptember <sergey_meshkov.ru@mail.ru>
2024-04-30 11:39:17 +0300

    Beginning

--- MirFood.Web/MirFood.Web.Client/Program.cs
+++ MirFood.Web/MirFood.Web.Client/Program.cs
@@ -1,12 +1,15 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
 
 namespace MirFood.Web.Client
 {
     internal class Program
     {
-        static async Task Main(string[] args)
+        private static async Task Main(string[] args)
         {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+            builder.Services.AddRadzenComponents();
 
             await builder.Build().RunAsync();
         }
--- MirFood.Web/MirFood.Web/Program.cs
+++ MirFood.Web/MirFood.Web/Program.cs
@@ -1,5 +1,6 @@
 using MirFood.Web.Client.Pages;
 using MirFood.Web.Components;
+using Radzen;
 
 namespace MirFood.Web
 {
@@ -7,13 +8,15 @@ namespace MirFood.Web
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddRadzenComponents();
 
             // Add services to the container.
             builder.Services.AddRazorComponents()
                 .AddInteractiveWebAssemblyComponents();
 
-            var app = builder.Build();
+            WebApplication app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
